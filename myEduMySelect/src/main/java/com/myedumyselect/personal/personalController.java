7d864fe6a0284745c2d39b25775456117f01c873
icand <icand@kangminho>
2024-02-26 20:48:34 +0900

    2024_02_26(개인회원 로그인 컨트롤러 추가)

--- myEduMySelect/src/main/java/com/myedumyselect/personal/personalController.java
+++ myEduMySelect/src/main/java/com/myedumyselect/personal/personalController.java
@@ -3,5 +3,6 @@ package com.myedumyselect.personal;
 public class personalController {
 	public static void main(String[] args) {
 		System.out.println("컨트롤러");
+		System.out.println("개인 회원 로그인 컨트롤러");
 	}
 }
