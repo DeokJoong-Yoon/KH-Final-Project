b74c10825c686dba8234dfcc74110b1137a7c17f
icand <icand@kangminho>
2024-02-26 20:51:05 +0900

    2024_02_26("강민호")

--- myEduMySelect/src/main/java/com/myedumyselect/personal/personalController.java
+++ myEduMySelect/src/main/java/com/myedumyselect/personal/personalController.java
@@ -4,5 +4,6 @@ public class personalController {
 	public static void main(String[] args) {
 		System.out.println("컨트롤러");
 		System.out.println("개인 회원 로그인 컨트롤러");
+		System.out.println("ㅎㅇㅎㅇ");
 	}
 }
