6e00d84d1827a1e0c3a28ac476491feb09b80451
dudals0921 <dudals0921@gmail.com>
2024-03-06 08:55:52 +0900

    20240306_최영민_academyPackage구조 관리

---
+++ myEduMySelect/src/main/java/com/myedumyselect/academy/dao/AcademyDao.java
@@ -0,0 +1,10 @@
+package com.myedumyselect.academy.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AcademyDao {
+	
+	 
+	
+}
--- myEduMySelect/src/main/java/com/myedumyselect/academy/vo/AcademyVo.java
+++ myEduMySelect/src/main/java/com/myedumyselect/academy/vo/AcademyVo.java
@@ -1,18 +1,8 @@
 package com.myedumyselect.academy.vo;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
-@ToString
-@Getter
-@Setter
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class AcademyVo {
 	private int academyNumber;
 	private String academyName;
