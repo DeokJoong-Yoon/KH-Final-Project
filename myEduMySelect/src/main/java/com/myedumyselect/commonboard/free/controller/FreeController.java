11a3825fb88be205317567c9feb8f2acba87ac6c
DeokJoong-Yoon <ykdj92@naver.com>
2024-03-24 21:43:05 +0900

    20240324_윤덕중 자유게시판 세션제한 로직 추가 완료.

--- myEduMySelect/src/main/java/com/myedumyselect/commonboard/free/controller/FreeController.java
+++ myEduMySelect/src/main/java/com/myedumyselect/commonboard/free/controller/FreeController.java
@@ -12,9 +12,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import com.myedumyselect.academy.vo.AcademyLoginVO;
+import com.myedumyselect.common.util.SessionCheckService;
 import com.myedumyselect.common.vo.PageDTO;
 import com.myedumyselect.commonboard.free.service.FreeService;
 import com.myedumyselect.commonboard.free.vo.FreeVO;
+import com.myedumyselect.personal.vo.PersonalLoginVO;
 
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -26,35 +31,57 @@ public class FreeController {
 	@Setter(onMethod_ = @Autowired)
 	private FreeService freeService;
 
-	
-	/* 글 목록 구현*/
+	@Setter(onMethod_ = @Autowired)
+	private SessionCheckService sessionCheckService;
+
+	/* 글 목록 구현 */
 	@GetMapping("/freeList")
-	public String freeList(@ModelAttribute FreeVO fvo, Model model) {
-		log.info("freeList 호출 성공");
-			
+	public String freeList(@ModelAttribute FreeVO fvo, Model model,
+			@SessionAttribute(required = false, value = "personalLogin") PersonalLoginVO personalLoginVO,
+			@SessionAttribute(required = false, value = "academyLogin") AcademyLoginVO academyLoginVO) {
+		if (academyLoginVO != null) {
+			String academyResult = sessionCheckService.isAcademySessionCheck(academyLoginVO, model);
+			if (academyResult != "TRUE") {
+				return academyResult;
+			}
+		} else if (personalLoginVO != null) {
+			String pesronalResult = sessionCheckService.isPersonalSessionCheck(personalLoginVO, model);
+			if (pesronalResult != "TRUE") {
+				return pesronalResult;
+			}
+		} else {
+			System.out.println("else");
+			return "redirect:/";
+		}
 		List<FreeVO> freeList = freeService.freeList(fvo);
 		model.addAttribute("freeList", freeList);
-		
+
 		int total = freeService.freeListCnt(fvo);
-		
-		log.info("keyword : "+ fvo.getKeyword());
-		log.info("search : "+ fvo.getKeyword());
+
+		log.info("keyword : " + fvo.getKeyword());
+		log.info("search : " + fvo.getKeyword());
 		model.addAttribute("pageMaker", new PageDTO(fvo, total));
-		
+
 		return "board/free/freeBoardList";
-		 
+
 	}
-	
-	
-	/* 글쓰기 폼 구현*/
+
+	/* 글쓰기 폼 구현 */
 	@GetMapping(value = "/freeWriterForm")
-	public String freeWriterForm() {
-		log.info("freeWriterForm 호출 성공");
+	public String freeWriterForm(Model model,
+			@SessionAttribute(required = false, value = "personalLogin") PersonalLoginVO personalLoginVO) {
+		if (personalLoginVO != null) {
+			String personalResult = sessionCheckService.isPersonalSessionCheck(personalLoginVO, model);
+			if (personalResult != "TRUE") {
+				return personalResult;
+			}
+		} else {
+			return "redirect:/";
+		}
 
 		return "board/free/freeWriterForm";
 	}
-	
-	
+
 	@PostMapping("/freeInsert")
 	public String freeInsert(FreeVO fvo) throws Exception {
 		log.info("freeInsert 호출 성공");
@@ -64,14 +91,26 @@ public class FreeController {
 		log.info(fvo.toString());
 		return "redirect:/free/freeList";
 	}
-	
 
-	
-	
-	/*글 상세 구현*/
+	/* 글 상세 구현 */
 	@GetMapping("/freeDetail")
-	public String freeDetail(@ModelAttribute FreeVO fvo, Model model) {
-		log.info("freeDetail 호출 성공");
+	public String freeDetail(@ModelAttribute FreeVO fvo, Model model,
+			@SessionAttribute(required = false, value = "personalLogin") PersonalLoginVO personalLoginVO,
+			@SessionAttribute(required = false, value = "academyLogin") AcademyLoginVO academyLoginVO) {
+		if (academyLoginVO != null) {
+			String academyResult = sessionCheckService.isAcademySessionCheck(academyLoginVO, model);
+			if (academyResult != "TRUE") {
+				return academyResult;
+			}
+		} else if (personalLoginVO != null) {
+			String pesronalResult = sessionCheckService.isPersonalSessionCheck(personalLoginVO, model);
+			if (pesronalResult != "TRUE") {
+				return pesronalResult;
+			}
+		} else {
+			System.out.println("else");
+			return "redirect:/";
+		}
 
 		FreeVO detail = freeService.freeDetail(fvo);
 		model.addAttribute("detail", detail);
@@ -79,47 +118,57 @@ public class FreeController {
 		return "board/free/freeBoardDetail";
 	}
 
-	
-	
-	//글 수정 구현
-	
-	@GetMapping(value="/freeUpdateForm")
-	public String freeUpdateForm(@ModelAttribute FreeVO fvo, Model model) {
-	    log.info("freeUpdateForm 호출 성공");
-	    log.info("commonNo = " + fvo.getCommonNo());
-	    
-	    FreeVO freeUpdateData = freeService.freeUpdateForm(fvo);
-	    
-	    model.addAttribute("freeUpdateData", freeUpdateData);
-	    return "board/free/freeUpdateForm";
+	// 글 수정 구현
+
+	@GetMapping(value = "/freeUpdateForm")
+	public String freeUpdateForm(@ModelAttribute FreeVO fvo, Model model,
+			@SessionAttribute(required = false, value = "personalLogin") PersonalLoginVO personalLoginVO) {
+		if (personalLoginVO != null) {
+			String personalResult = sessionCheckService.isPersonalSessionCheck(personalLoginVO, model);
+			if (personalResult != "TRUE") {
+				return personalResult;
+			}
+		} else {
+			return "redirect:/";
+		}
+
+		FreeVO freeUpdateData = freeService.freeUpdateForm(fvo);
+
+		model.addAttribute("freeUpdateData", freeUpdateData);
+		return "board/free/freeUpdateForm";
 	}
 
-	
-
 	@PostMapping("/freeUpdate")
-	public String freeUpdate(@ModelAttribute FreeVO fvo) throws Exception{
+	public String freeUpdate(@ModelAttribute FreeVO fvo) throws Exception {
 		log.info("freeUpdate 호출 성공");
 		freeService.freeUpdate(fvo);
 		return "redirect:/free/freeList";
 	}
-	
+
 	@GetMapping("/freeDelete")
-	public String freeDelete(@ModelAttribute FreeVO fvo) throws Exception{
-		log.info("freeDelete 호출 성공");
+	public String freeDelete(@ModelAttribute FreeVO fvo, Model model,
+			@SessionAttribute(required = false, value = "personalLogin") PersonalLoginVO personalLoginVO)
+			throws Exception {
+		if (personalLoginVO != null) {
+			String personalResult = sessionCheckService.isPersonalSessionCheck(personalLoginVO, model);
+			if (personalResult != "TRUE") {
+				return personalResult;
+			}
+		} else {
+			return "redirect:/";
+		}
 		freeService.freeDelete(fvo);
 		return "redirect:/free/freeList";
 	}
-	
-	
-	
+
 	@ResponseBody
-	@PostMapping(value="/freereplyCount", produces=MediaType.TEXT_PLAIN_VALUE)
+	@PostMapping(value = "/freereplyCount", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String freereplyCount(@RequestParam("commonNo") int commonNo) {
 		log.info("freereplyCount 호출 성공");
-		
+
 		int result = 0;
 		result = freeService.freeReplyCnt(commonNo);
-		
+
 		return String.valueOf(result);
 	}
 
