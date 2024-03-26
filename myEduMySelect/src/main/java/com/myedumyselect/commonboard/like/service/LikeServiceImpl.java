551e27a8d12e106da0b5874d1347e0aa73193f3a
user1 <user1@DESKTOP-F2CL4UO>
2024-03-26 12:43:59 +0900

    20240326 김시온 홍보게시판 찜 수정

--- myEduMySelect/src/main/java/com/myedumyselect/commonboard/like/service/LikeServiceImpl.java
+++ myEduMySelect/src/main/java/com/myedumyselect/commonboard/like/service/LikeServiceImpl.java
@@ -9,13 +9,13 @@ import com.myedumyselect.commonboard.like.vo.LikeVO;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
-@Slf4j
 public class LikeServiceImpl implements LikeService {
 	
 	@Autowired
 	private LikeDAO ldao;
 
 
+	//최초 좋아요 처리
 	@Override
 	public int insertLike(LikeVO lvo) {
 		int result = 0;
@@ -24,6 +24,7 @@ public class LikeServiceImpl implements LikeService {
 	}
 
 
+	//이후 좋아요 취소/등록 토글
 	@Override
 	public int toggleLike(LikeVO lvo) {
 		int result = 0;
@@ -33,12 +34,7 @@ public class LikeServiceImpl implements LikeService {
 	}
 
 	
-//	@Override
-//	public int getLikeCount(int commonNo) {
-//		return ldao.getLikeCount(commonNo);
-//	}
-	
-	
+	//좋아요 상태 확인
 	@Override
 	public Integer getLike(LikeVO lvo) {
 		Integer result = ldao.getLike(lvo);
